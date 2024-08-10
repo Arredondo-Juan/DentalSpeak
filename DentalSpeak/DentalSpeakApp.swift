643a13e33f42798d80aa5a77681df83ba3f53635
Juan Arredondo <juan_arredondo@icloud.com>
2024-08-09 20:10:01 -0500

    Initial Commit

---
+++ DentalSpeak/DentalSpeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  DentalSpeakApp.swift
+//  DentalSpeak
+//
+//  Created by Juan Arredondo on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct DentalSpeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
