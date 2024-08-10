94d57a739a79ac64f2a5a444662c719c9a7963a5
Arredondo-Juan <juan_arredondo@icloud.com>
2024-08-10 10:04:02 -0500

    - make app functional

--- DentalSpeak/DentalSpeakApp.swift
+++ DentalSpeak/DentalSpeakApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct DentalSpeakApp: App {
+    @StateObject private var flashcardViewModel = FlashcardViewModel()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(flashcardViewModel)
         }
     }
 }
+
